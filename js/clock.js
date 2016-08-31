4625ed96d9943f029eba2e7520667d506b4c46af
Russell Brown <russellbrowncompany@gmail.com>
2016-08-31 16:34:13 -0500

    Changed some of the javascript to better handle the dates. I wasn't correct on the calculations using % operator. I think this better handles the months and years. I've tested it and it seems to work. Anybody's input would be apprreciated

--- js/clock.js
+++ js/clock.js
@@ -27,13 +27,20 @@ function clock_update() {
 	
 	//This is used to get the number of years since the last press conference
 	var years_since_press_conference = year - p_year; 
-	var years_since_press_conference_display = years_since_press_conference - 1; 
-	
+	var years_since_press_conference_display;
+
+	if (month + p_month >= 12) {
+		years_since_press_conference_display = years_since_press_conference - 1;
+	} else {
+		years_since_press_conference_display = years_since_press_conference; 
+	}
+
+
 	//This is used to get the number of months from the last press conference but discounted for the years since. This would factor in the number of years. 
-	var months_since_press_conference = 12 - p_month + month;
-	
+	var months_since_press_conference = ((12 - p_month) + month)%12;	
+		
 	//This will take the number of days in the month minus the date of the press conference to get the number of days since the last press conference. This is using the absoulte value of the resulting number to display the correct number of days. 
-	var days_since_press_conference = Math.abs(p_day - day);
+	var days_since_press_conference =  day - p_day;
 
 	// This is a simple calculation to get the minutes from the top of the hour. I didn't have the exact press conference start time so, I assumed it started at the top of the hour. 
 	var minutes_since_press_conference = minute - p_minute;
@@ -43,12 +50,13 @@ function clock_update() {
 
 	
 	// Print the information to the screen using getElementById method
-	document.getElementById('years').innerHTML = years_since_press_conference_display;
-	document.getElementById('months').innerHTML = months_since_press_conference;
-	document.getElementById('days').innerHTML = days_since_press_conference;
+	document.getElementById('years').innerHTML   = years_since_press_conference_display;
+	document.getElementById('months').innerHTML  = months_since_press_conference;
+	document.getElementById('days').innerHTML    = days_since_press_conference;
 	document.getElementById('minutes').innerHTML = minutes_since_press_conference;
 	document.getElementById('seconds').innerHTML = seconds_since_press_conference;
 	
 	// This will update the time once per second
 	setTimeout(clock_update, 1000)
 }
+
